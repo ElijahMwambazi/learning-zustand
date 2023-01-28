dbf60608c8d8b9d44dbfaa2c213aa7b49ad4a604
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2023-01-28 06:05:54 +0200

    Added course list component

--- src/components/cousre-list.tsx
+++ src/components/cousre-list.tsx
@@ -1,7 +1,56 @@
-// import { Fragment } from "react";
+import { Fragment } from "react";
+
+import useCourseStore from "../stores/course-store.store";
 
 const CourseList = () => {
-  return <></>;
+  const {
+    courses,
+    removeCourse,
+    toggleCourseStatus,
+  } = useCourseStore();
+
+  return (
+    <>
+      <ul>
+        {courses.map((course) => {
+          return (
+            <Fragment key={course.id}>
+              <li
+                className={`course-item`}
+                style={{
+                  backgroundColor:
+                    course.completed
+                      ? "green"
+                      : "white",
+                }}
+              >
+                <span className="course-item-col-1">
+                  <input
+                    checked={course.completed}
+                    type="checkbox"
+                    onChange={(e) => {
+                      toggleCourseStatus(
+                        course.id
+                      );
+                    }}
+                  />
+                </span>
+                <span>{course?.title}</span>
+                <button
+                  onClick={() => {
+                    removeCourse(course.id);
+                  }}
+                  className="delete-btn"
+                >
+                  Delete
+                </button>
+              </li>
+            </Fragment>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default CourseList;
